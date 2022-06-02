5f050f058cf001fd586988323bcc41e26f7ce645
Breach <breach344@gmail.com>
2022-06-02 01:02:58 -0700

    Update header.php

--- components/header.php
+++ components/header.php
@@ -11,7 +11,8 @@
             <a href="../../index.php" class="h-links">Home</a>
             <a href="../../pages/news.php" class="h-links">News</a>
             <a href="../../pages/calendar.php" class="h-links">Calendar</a>
-            <a href="../../pages/about.php" class="h-links">About Us &nbsp;</a>
+            <a href="../../pages/about.php" class="h-links">About Us</a>
+            <a href="../../login.php" class="h-links">Login &nbsp;</a>
         </div>
     </div>
 </header>
